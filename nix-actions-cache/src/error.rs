d8fbc93e4f25d7608875ba30c8ae0e21f530422d
Luc Perkins <lucperkins@gmail.com>
2023-06-22 11:57:02 -0700

    Fix Clippy issues and add Rust checks to CI

--- nix-actions-cache/src/error.rs
+++ nix-actions-cache/src/error.rs
@@ -1,21 +1,17 @@
 //! Errors.
 
-use std::io::Error as IoError;
-
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 use thiserror::Error;
 
-use gha_cache::api::Error as ApiError;
-
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("GitHub API error: {0}")]
-    ApiError(#[from] ApiError),
+    Api(#[from] gha_cache::api::Error),
 
     #[error("Not Found")]
     NotFound,
@@ -24,7 +20,7 @@ pub enum Error {
     BadRequest,
 
     #[error("I/O error: {0}")]
-    IoError(#[from] IoError),
+    Io(#[from] std::io::Error),
 
     #[error("Failed to upload paths")]
     FailedToUpload,
@@ -34,7 +30,7 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         let code = match &self {
             // HACK: HTTP 418 makes Nix throw a visible error but not retry
-            Self::ApiError(_) => StatusCode::IM_A_TEAPOT,
+            Self::Api(_) => StatusCode::IM_A_TEAPOT,
             Self::NotFound => StatusCode::NOT_FOUND,
             Self::BadRequest => StatusCode::BAD_REQUEST,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
