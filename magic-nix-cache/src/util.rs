02061a58501ca8d1ef26e58e857244fc86ebe1bb
Cole Helbling <cole.helbling@determinate.systems>
2024-05-14 15:13:50 -0700

    wip: use Nix's sqlite db "directly"
    this might not be a safe assumption, but we're gonna try

--- magic-nix-cache/src/util.rs
+++ magic-nix-cache/src/util.rs
@@ -9,34 +9,19 @@ use crate::error::Result;
 
 /// Returns the list of store paths that are currently present.
 pub async fn get_store_paths(store: &NixStore) -> Result<HashSet<PathBuf>> {
-    // FIXME: use the Nix API.
-    let store_dir = store.store_dir();
-    let mut listing = tokio::fs::read_dir(store_dir).await?;
-    let mut paths = HashSet::new();
-    while let Some(entry) = listing.next_entry().await? {
-        let file_name = entry.file_name();
-        let file_name = Path::new(&file_name);
+    // FIXME(cole-h): update the nix bindings to get the dbdir of the localstore?
+    let db =
+        rusqlite::Connection::open("file:/nix/var/nix/db/db.sqlite?immutable=1").expect("FIXME");
 
-        if let Some(extension) = file_name.extension() {
-            match extension.to_str() {
-                None | Some("drv") | Some("chroot") => {
-                    tracing::debug!(
-                        "skipping file with weird or uninteresting extension {extension:?}"
-                    );
-                    continue;
-                }
-                _ => {}
-            }
-        }
+    let mut stmt = db.prepare("SELECT path FROM ValidPaths").expect("FIXME");
+    let paths = stmt
+        .query_map([], |row| -> std::result::Result<PathBuf, rusqlite::Error> {
+            Ok(PathBuf::from(row.get::<_, String>(0)?))
+        })
+        .expect("FIXME")
+        .into_iter()
+        .map(|r| r.expect("FIXME"))
+        .collect::<HashSet<_>>();
 
-        if let Some(s) = file_name.to_str() {
-            // Special paths (so far only `.links`)
-            if s == ".links" {
-                continue;
-            }
-        }
-
-        paths.insert(store_dir.join(file_name));
-    }
     Ok(paths)
 }
