6240079bc60c2402d11467e58e2699b6e44d67f1
Cole Helbling <cole.helbling@determinate.systems>
2026-08-10 13:26:47 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix.url = "https://flakehub.com/f/DeterminateSystems/nix-src/=3.16.3";
+    nix.url = "https://flakehub.com/f/DeterminateSystems/nix-src/=3.22.0";
   };
 
   outputs =
